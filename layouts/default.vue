b69fb373d4d7131caf2cad25754db477087d98a1
chudickgumanoid <sashaq0708@gmail.com>
2025-07-08 19:20:01 +0500

    fix: adaptive bg

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,22 +14,34 @@ import { LazyMainFooter } from "#components";
 
 <style scoped>
 .layout-wrapper {
+  position: relative;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  z-index: 1;
+}
 
+.layout-wrapper::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background-image: url("/img/background.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-attachment: fixed;
+  z-index: -1;
+  pointer-events: none;
 }
 
 @media (max-width: 500px) {
-  .layout-wrapper {
+  .layout-wrapper::before {
     background-image: url("/img/background-mobile.jpg");
   }
 }
+
 .content {
   flex: 1;
   padding: 60px 0 24px 0;
